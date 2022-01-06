915d63a4760fd9b1d7104705ab3fe9f8197e0e33
thuluyen95 <vuluyen.hanu@gmail.com>
2022-01-06 18:04:51 +0700

    bai tap bai 7 da sua

---
+++ bt bài 7/Bai 7_BT3 đổi từ độ f sang độ c.js
@@ -0,0 +1,3 @@
+let f = prompt('xin moi nhap do f');
+let c = (f-32)/1.8;
+console.log(Math.round(c));
---
+++ bt bài 7/bai 7_BT1 tính căn bậc 2.js
@@ -0,0 +1,3 @@
+let numberOne = prompt('moi nhap chu so bat ky');
+let canbachaiso1 = Math.sqrt(numberOne);
+console.log(canbachaiso1);
